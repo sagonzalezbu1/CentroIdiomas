d2c42584423c35c507d9876cc658590e04f1b97d
Lsuarez08 <51854924+Lsuarez08@users.noreply.github.com>
2019-06-19 15:24:00 -0500

    versolicitud del estudiante (metodo)

--- src/gestorAplicacion/usuarios/Estudiante.java
+++ src/gestorAplicacion/usuarios/Estudiante.java
@@ -3,84 +3,98 @@ package gestorAplicacion.usuarios;
 import java.util.*;
 import gestorAplicacion.Centro.*;
 
-public class Estudiante extends Usuario{  
-    private String estado = "Inactivo";
-    private ArrayList <Certificado> misCertificados = new ArrayList<Certificado>();
-    private ArrayList <Solicitud> misSolicitudes = new ArrayList<Solicitud>();
-    private ArrayList <Curso> misCursos = new ArrayList<Curso>();
-    private HashMap<String, ArrayList<Float>> misNotas = new HashMap();
-    
-    public Estudiante(String name, long cedula, String correo, String clave){
-        super(name,cedula,correo,clave);
-    }
-    
-    public String miHorario(){
-        String aux = "";
-        for (Curso x:misCursos){
-            aux += x + "\n";
-        }
-        return aux;
-    }
-    public String getEstado(){
-        return estado;
-    }
-    //0 para matricular, 1 para finalizar
-    public void setEstado(int status){
-        if(status == 0){
-            estado = "Activo";
-        }
-    }
-    public void setEstado(String mater, int status){   
-        if(status == 1){
-            if(misCursos.size()>1){
-                estado = "Activo";
-            }
-            else{
-                float aux = this.getNota(mater);
-                if(aux>=3.0){
-                    estado = "Egresado";
-                }
-                else{
-                    estado = "Inactivo";
-                }
-            }
-        }    
-    }
-    public float getNota(String nombreM){
-        ArrayList<Float> prom = misNotas.get(nombreM);
-        float def = 0;
-        for (float x:prom){
-            def += x;
-        }
-        def = def/prom.size();
-        return def;
-    }
-    
-    public void addCurso(Curso course){
-        misCursos.add(course);
-    }
-    public void removeCurso(String nombreCurso){
-        for (Curso x : misCursos){
-            if(x.getNombreCurso().equals(nombreCurso)){
-                misCursos.remove(x);
-            }
-        }
-    }
-    public void addCertificado(Certificado certify){
-        misCertificados.add(certify);
-    }
-        
-    public void calificar(String name, float grade){
-            ArrayList<Float> itemsList = misNotas.get(name);
-            itemsList.add(grade);
-    }
-    public void adSolicitud(Solicitud reg) {
-    	misSolicitudes.add(reg);
-    }
-    public void removeSolicitud(Solicitud soli) {
-    	misSolicitudes.remove(soli);
-    }
-    public String toString(){
-        return ("Nombre: " + super.nombre + "\nCedula: " + super.cedula);
-    }
+public class Estudiante extends Usuario {
+	private String estado = "Inactivo";
+	private ArrayList<Certificado> misCertificados = new ArrayList<Certificado>();
+	private ArrayList<Solicitud> misSolicitudes = new ArrayList<Solicitud>();
+	private ArrayList<Curso> misCursos = new ArrayList<Curso>();
+	private HashMap<String, ArrayList<Float>> misNotas = new HashMap<>();
+
+	public Estudiante(String name, long cedula, String correo, String clave) {
+		super(name, cedula, correo, clave);
+	}
+
+	public String miHorario() {
+		String aux = "";
+		for (Curso x : misCursos) {
+			aux += x + "\n";
+		}
+		return aux;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	// 0 para matricular, 1 para finalizar
+	public void setEstado(int status) {
+		if (status == 0) {
+			estado = "Activo";
+		}
+	}
+
+	public void setEstado(String mater, int status) {
+		if (status == 1) {
+			if (misCursos.size() > 1) {
+				estado = "Activo";
+			} else {
+				float aux = this.getNota(mater);
+				if (aux >= 3.0) {
+					estado = "Egresado";
+				} else {
+					estado = "Inactivo";
+				}
+			}
+		}
+	}
+
+	public float getNota(String nombreM) {
+		ArrayList<Float> prom = misNotas.get(nombreM);
+		float def = 0;
+		for (float x : prom) {
+			def += x;
+		}
+		def = def / prom.size();
+		return def;
+	}
+
+	public void addCurso(Curso course) {
+		misCursos.add(course);
+	}
+
+	public void removeCurso(String nombreCurso) {
+		for (Curso x : misCursos) {
+			if (x.getNombreCurso().equals(nombreCurso)) {
+				misCursos.remove(x);
+			}
+		}
+	}
+
+	public void addCertificado(Certificado certify) {
+		misCertificados.add(certify);
+	}
+
+	public void calificar(String name, float grade) {
+		ArrayList<Float> itemsList = misNotas.get(name);
+		itemsList.add(grade);
+	}
+
+	public void adSolicitud(Solicitud reg) {
+		misSolicitudes.add(reg);
+	}
+
+	public void removeSolicitud(Solicitud soli) {
+		misSolicitudes.remove(soli);
+	}
+	
+	public String verSolicitudes() {
+		String aux = "";
+		for (Solicitud x : misSolicitudes) {
+			aux += x + ", ";
+		}
+		return aux;
+	}
+	public String toString() {
+		return ("Nombre: " + super.nombre + "\nCedula: " + super.cedula);
+	}
 }
