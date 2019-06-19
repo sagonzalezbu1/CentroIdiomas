2d1d4bfbda1baa93034ed51884ddd9155c7d6a0d
sagonzalezbu1 <51854812+sagonzalezbu1@users.noreply.github.com>
2019-06-19 13:57:58 -0500

    Subí la clase "Certificado" y "curso"

--- src/gestorAplicacion/Centro/Certificado.java
+++ src/gestorAplicacion/Centro/Certificado.java
@@ -1,5 +1,20 @@
 package gestorAplicacion.Centro;
 
 public class Certificado {
+	final private String nombre;
+    final private String alumno;
+    final private float nota;
+    final private Docente docente;
+
+    Certificado(String name, String alumno, Docente docente, float nota) {
+        nombre = name;
+        this.alumno = alumno;
+        this.docente = docente;
+        this.nota = nota;
+    }
+
+    public String toString() {
+        return "El estudiante " + alumno + " ha aprobado el curso de " + nombre + " con una nota de " + nota + " \n con el profesor " + docente.getNombre();
+    }
 
 }
