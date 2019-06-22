28d594ac8b361f32fa1150d37a43b90569620da0
sagonzalezbu1 <51854812+sagonzalezbu1@users.noreply.github.com>
2019-06-22 17:05:48 -0500

    Cree una lista con las funcionalidades en el main

--- src/uiMain/Main.java
+++ src/uiMain/Main.java
@@ -1,10 +1,32 @@
 package uiMain;
 
-import gestorAplicacion.usuarios.*;
+import java.util.*;
 
+import gestorAplicacion.usuarios.*;
 public class Main {
 	public static Usuario user= new Invitado();
 	public static void main(String[] args) {
+		ArrayList<OpcionDeMenu> funcionalidadess = new ArrayList<OpcionDeMenu>();
+		funcionalidadess.add(new AceptarSolicitudes());
+		funcionalidadess.add(new AddFuncionalidad());
+		funcionalidadess.add(new Calificar());
+		funcionalidadess.add(new CrearCurso());
+		funcionalidadess.add(new CursosDisponibles());
+		funcionalidadess.add(new FinalizeCurso());
+		funcionalidadess.add(new Ingresar());
+		funcionalidadess.add(new Matricular());
+		funcionalidadess.add(new RechazarSolicitudes());
+		funcionalidadess.add(new Registrar());
+		funcionalidadess.add(new RemoveFuncionalidad());
+		funcionalidadess.add(new SolicitarCupo());
+		funcionalidadess.add(new VerAdministradores());
+		funcionalidadess.add(new VerCertificados());
+		funcionalidadess.add(new VerDocentes());
+		funcionalidadess.add(new VerEstudiantes());
+		funcionalidadess.add(new VerHorario());
+		funcionalidadess.add(new VerMisSolicitudes());
+		funcionalidadess.add(new VerNotas());
+		
 		while(true) {
 			user.getMenu().lanzarMenu();
 		}
