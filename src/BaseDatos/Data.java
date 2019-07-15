4c79b2dc9d82c0099ace722e07876bd05f7cf2db
Lsuarez08 <51854924+Lsuarez08@users.noreply.github.com>
2019-07-15 15:07:06 -0500

    cursos.txt

--- src/BaseDatos/Data.java
+++ src/BaseDatos/Data.java
@@ -125,7 +125,7 @@ public class Data {
 	public static void cargarCursos() {
 		try {
 			String direccion = System.getProperty("user.dir") + "\\src\\temp\\";
-			File f = new File(direccion + "docentes.txt");
+			File f = new File(direccion + "cursos.txt");
 			FileReader fr = new FileReader(f);
             BufferedReader br = new BufferedReader(fr);
             String linea;
